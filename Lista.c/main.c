0da55e03a694140b58b1a1195be70d51bb672b3b
BarraFrancesco <55709012+BarraFrancesco@users.noreply.github.com>
2020-03-14 16:27:26 +0100

    Add files via upload

---
+++ Lista.c/main.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "nodo.h"
+
+void stampa(Nodo*head);
+
+int main(int argc, char *argv[]) {
+	Nodo*head=NULL;
+	stampa(head);
+	
+	Nodo n1;
+	n1.val=3;
+	n1.next=NULL;
+	
+	head=&n1;
+	
+	stampa(head);
+	
+	Nodo n2;
+	n2.val=7;
+	n2.next=NULL;
+	
+	n1.next=&n2;
+	
+	stampa(head);
+	
+		Nodo n3;
+		n3.val=10;
+		n3.next=NULL;
+		
+		n3.next=head;
+		head=&n3;
+		
+		stampa (head);
+	
+	return 0;
+	
+	void stampa(Nodo *head)
+{
+    Nodo *temp;
+
+    temp = head;
+    while (temp != NULL) {
+        printf("%d ---> ", temp->val);
+        temp = temp->next;
+    }
+    printf("NULL\n");
+}
+}
